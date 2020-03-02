277e40f3699a2a1463a955bc2b07267df866deae
jovis18 <jovis18@sdu.student.dk>
2020-03-02 21:15:22 +0100

    test added

---
+++ systest.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include "arch/x86/include/generated/uapi/asm/unistd_64.h"
+#include <stdlib.h>
+
+int main(int argc, char ** arv){
+	char* str = "Hello World";
+	printf("%li\n", syscall(__NR_dm510_msgbox_put, str, strlen(str)));
+	printf("succes on put\n");
+
+	printf("Now to get\n");
+	char* buffer = "To be overwritten my friend"; //malloc(sizeof(char)*32);
+	printf("%li\n", syscall(__NR_dm510_msgbox_get, buffer, strlen(buffer)));
+	for(int i = 0; i<32 && buffer[i]!='\0'; i++){
+		printf("%c", buffer[i]);
+	}
+	printf("\n");
+}
+
