314d3c5bbed231570c18873082daee2bbc7b1ad8
jovis18 <jovis18@sdu.student.dk>
2020-03-03 16:09:33 +0100

    added error codes

--- dm510_msgbox.c
+++ dm510_msgbox.c
@@ -21,25 +21,25 @@ int sys_dm510_msgbox_put(char *buffer, int length){
 	unsigned long flags;
 
 	if(length<0){
-		return 1;	//cant copy negative length
+		return EINVAL;	//cant copy negative length
 	}
 	ptr = kmalloc(sizeof(msg_t), GFP_KERNEL);
 	if(!ptr){
-		return 2;	//if malloc fails, malloc return a null pointer
+		return ENOMEM;	//if malloc fails, malloc return a null pointer
 	}
 	msg = ptr;
 	msg->previous = NULL;
 	msg->length = length;
 	ptr = kmalloc(length, GFP_KERNEL);
 	if(!ptr){
-		return 3;	//if malloc fails, malloc returns a null pointer
+		return ENOMEM;	//if malloc fails, malloc returns a null pointer
 	}
 	msg->message = ptr;
 	if(access_ok(buffer,length)==0){
-		return 4;	//If data could not be read or written access_ok returns 0
+		return ENOBUFS;	//Buffer space not available
 	}
 	if(copy_from_user(msg->message, buffer, length)!=0){
-		return 5;	//If the copy could not copy all the bytes, it will return the number of failed bytes, on succes return 0
+		return EIO;	//If the copy could not copy all the bytes, it will return the number of failed bytes, on succes return 0
 	}
 
 	local_irq_save(flags);
@@ -62,7 +62,7 @@ int sys_dm510_msgbox_get(char *buffer, int length){
 	local_irq_save(flags);
 	if(top==NULL){
 		local_irq_restore(flags);
-		return 1;
+		return ENODATA;
 	}
 	msg = top;
 	mlength = msg->length;
@@ -71,15 +71,15 @@ int sys_dm510_msgbox_get(char *buffer, int length){
 	local_irq_restore(flags);
 
 	if(mlength>length){
-		return 2;
+		return ENOMEM;		//Not enough space
 	}
 
 	if(access_ok(buffer, mlength)==0){
-		return 3;
+		return ENOBUFS;		//Buffer space not available
 	}
 
 	if(copy_to_user(buffer, msg->message, mlength)!=0){
-		return 4;
+		return EIO;
 	}
 
 
