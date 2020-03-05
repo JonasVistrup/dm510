ff73c4616a973fed235835f9187e04d3623044e5
mihej18 <mihej18@student.sdu.dk>
2020-03-05 15:43:10 +0100

    Added KFree and stuff

--- dm510_msgbox.c
+++ dm510_msgbox.c
@@ -21,34 +21,45 @@ int sys_dm510_msgbox_put(char *buffer, int length){
 	unsigned long flags;
 
 	if(length<0){
-		return EINVAL;	//cant copy negative length
+		return -EINVAL;	//cant copy negative length
 	}
+
+	if(access_ok(buffer,length)==0){
+                return -ENOBUFS; //Buffer space not available
+        }
+
 	ptr = kmalloc(sizeof(msg_t), GFP_KERNEL);
 	if(!ptr){
-		return ENOMEM;	//if malloc fails, malloc return a null pointer
+		return -ENOMEM;	//if malloc fails, malloc return a null pointer
 	}
+
+
 	msg = ptr;
 	msg->previous = NULL;
 	msg->length = length;
 	ptr = kmalloc(length, GFP_KERNEL);
 	if(!ptr){
-		return ENOMEM;	//if malloc fails, malloc returns a null pointer
+        	kfree(msg);
+		return -ENOMEM;	//if malloc fails, malloc returns a null pointer
 	}
 	msg->message = ptr;
-	if(access_ok(buffer,length)==0){
-		return ENOBUFS;	//Buffer space not available
-	}
+
+
 	if(copy_from_user(msg->message, buffer, length)!=0){
-		return EIO;	//If the copy could not copy all the bytes, it will return the number of failed bytes, on succes return 0
+		kfree(msg->message);
+        	kfree(msg);
+		return -EIO;	//If the copy could not copy all the bytes, it will return the number of failed bytes, on succes return 0
 	}
 
 	local_irq_save(flags);
+
 	if(top==NULL){
 		top = msg;
 	}else{
 		msg->previous = top;
 		top = msg;
 	}
+
 	local_irq_restore(flags);
 	return 0;
 }
@@ -62,30 +73,32 @@ int sys_dm510_msgbox_get(char *buffer, int length){
 	local_irq_save(flags);
 	if(top==NULL){
 		local_irq_restore(flags);
-		return ENODATA;
+		return -ENODATA;
 	}
 	msg = top;
 	mlength = msg->length;
-	top = msg->previous;
-
-	local_irq_restore(flags);
 
 	if(mlength>length){
-		return ENOMEM;		//Not enough space
+		local_irq_restore(flags);
+		return -ENOMEM;		//Not enough space
 	}
 
 	if(access_ok(buffer, mlength)==0){
-		return ENOBUFS;		//Buffer space not available
-	}
+		local_irq_restore(flags);
+                return -ENOBUFS;                //Buffer space not available
+        }
 
 	if(copy_to_user(buffer, msg->message, mlength)!=0){
-		return EIO;
+		local_irq_restore(flags);
+		return -EIO;
 	}
 
+	top = msg->previous;
+
+        local_irq_restore(flags);
 
 	/* free memory */
 	kfree(msg->message);
 	kfree(msg);
-
 	return mlength;
 }
