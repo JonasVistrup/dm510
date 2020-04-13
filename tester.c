f03f32e0e5f1f970a5bc9a7591bbb65088d65537
mihej18 <mihej18@student.sdu.dk>
2020-04-13 11:57:38 +0200

    More prints & defining magic number

--- tester.c
+++ tester.c
@@ -33,13 +33,17 @@ int devOpen(int index, int read, int write){
 	}
 }
 
-#define IOC_B_READ_SIZE         _IOW('k', 0, int)
-#define IOC_B_WRITE_SIZE        _IOW('k', 1, int)
-#define IOC_NUM_OF_READERS      _IOW('k', 2, int)
+#define IOC_MAGIC 'k'
+#define IOC_B_READ_SIZE _IOW(IOC_MAGIC, 0, int)
+#define IOC_B_WRITE_SIZE _IOW(IOC_MAGIC, 1, int)
+#define IOC_NUM_OF_READERS _IOW(IOC_MAGIC, 2, int)
 
 
 int main()
 {
+    printf("%s\n","");
+
+    int size = 5;
 
     int device_0 = devOpen(0,1,0);
     int device_1 = devOpen(1,0,1);
@@ -48,51 +52,54 @@ int main()
 	printf("Fail Open, Error: %d\n",device_0);
 	return 1;
     }else{
-    printf("Succesfull open\n");
+    printf("Succesfull open device_0\n");
     }
 
     if(device_1 < 0){
 	printf("Fail Open, Error: %d\n",device_1);
     }else{
-    printf("Succesfull open\n");
+    printf("Succesfull open device_1\n");
     }
+    printf("%s\n","");
 
+    int rc;
 
-
-
-
-    //ioctl(device_1,IOC_B_WRITE_SIZE, 3);
+    rc = ioctl(device_0,IOC_B_WRITE_SIZE, size);
 
 
     //Trying to write:
     int writenbytes = write(device_1, "Test", strlen("Test"));
-    printf("The number of bytes written is: %d\n", writenbytes);
+    printf("The number of bytes written by device_1:  %d\n\n", writenbytes);
     char* buffer = malloc(sizeof(char)*32);
 
-    ioctl(device_1,IOC_B_WRITE_SIZE, 3);
+//    rc = ioctl(device_1,IOC_B_WRITE_SIZE, size);
+
+    printf("Return value from ioctl:  %d\n\n", rc);
 
     //Trying to read:
     int readbytes = read(device_0, buffer, 4);
-    printf("The number of bytes read is: %d\n", readbytes);
+    printf("The number of bytes read to device_0:  %d\n", readbytes);
     //Printing out buffer:
-    printf("%s\n", buffer);
+    printf("Content of buffer read from:  %s\n\n", buffer);
 
+    free(buffer);
 
     //Trying to close:
     int closeID0 = close(device_0);
     int closeID1 = close(device_1);
+
     if(closeID0){
         printf("Fail Close, Error: %d\n",closeID0);
     }else{
-      printf("Succes Close\n");
+      printf("Succesfull Close device_0\n");
     }
 
     if(closeID1){
         printf("Fail Close, Error: %d\n",closeID1);
     }else{
-     printf("Succes Close\n");
+     printf("Succesfull Close device_1\n");
     }
-
+    printf("%s\n","");
 
     return 0;
 }
