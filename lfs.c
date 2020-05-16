e067324c255ff350c639eb485065a627c9dfcfee
vongroes <48790438+vongroes@users.noreply.github.com>
2020-05-16 12:44:58 +0200

    Update lfs.c

--- lfs.c
+++ lfs.c
@@ -52,7 +52,7 @@ int lfs_getattr( const char *path, struct stat *stbuf ) {
 
 	if(node == NULL){
 	printf("Exiting lfs_getattr: node == NULL\n");
-		return 0;
+		return ENOENT;
 	}
 
 	stbuf->st_size = (off_t) node->inode.size;
